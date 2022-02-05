17acb0bea62d6a86185501b7a96db93c32114a5c
silvasabrina <sabrina.silva@estudante.ufcg.edu.br>
2022-02-05 20:53:32 -0300

    Desafio Sentença e Bloco

---
+++ inicio/desafio_senteça_bloco.js
@@ -0,0 +1,19 @@
+/*
+exibir no console os numeros: 1 3 5 7 9
+e 2 4 6 8 10
+*/
+{
+    console.log( "1");
+    console.log( "3");
+    console.log( "5");
+    console.log( "7");
+    console.log( "9");
+}
+
+{
+    console.log( "2");
+    console.log( "4");
+    console.log( "6");
+    console.log( "8");
+    console.log( "10");
+}
