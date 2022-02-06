3d28f533a5fcf6c6e995be4c47c45e1dfc4cb26f
silvasabrina <sabrina.silva@estudante.ufcg.edu.br>
2022-02-06 12:30:54 -0300

    Trabalhando com Dados #2

---
+++ dados_2.js
@@ -0,0 +1,14 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+
+let nome = "Caderno"; //Strig (Texto)-> sequência de símbolos
+let categoria = "Papelaria";
+console.log("Produto: " + nome + ", Categoria: " + categoria 
++ ", Preço:  " + preco 
++ ", Desconto:   " + desconto);
+
+console.log(1+1);
+console.log("1"+1);
