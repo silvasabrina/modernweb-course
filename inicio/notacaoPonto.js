ad72a540be2a645431ec1fcba223974cd1597e57
silvasabrina <sabrina.silva@estudante.ufcg.edu.br>
2022-02-15 21:35:41 -0300

    Notação Ponto

---
+++ inicio/notacaoPonto.js
@@ -0,0 +1,12 @@
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+console.log(obj1.nome)
+
+function Obj(nome){
+    this.nome = nome
+    this.exec = function() {
+        console.log(exec)
+    }
+}
