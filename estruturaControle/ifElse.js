aaa94425e377b1403b10ea105c157cf1152c50de
silvasabrina <sabrina.silva@estudante.ufcg.edu.br>
2022-02-18 17:49:17 -0300

    Usando a Estrutura IF/ELSE

---
+++ estruturaControle/ifElse.js
@@ -0,0 +1,11 @@
+const imprimirResultado = function(num){
+    if(num >= 7) {
+        console.log('Aprovado!')
+    } else{
+        console.log('Reprovado!')
+    }
+}
+imprimirResultado(5)
+imprimirResultado(10)
+imprimirResultado('epa') //cuidado!!!
+
